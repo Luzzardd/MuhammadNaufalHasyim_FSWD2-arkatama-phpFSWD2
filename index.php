c1df98d0237fd3c18f59968a971da5e79ac875a3
Luzzardd <mnaufalhasyim112@gmail.com>
2023-04-15 12:40:00 +0700

    biodata

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+
+    <title>BIODATA</title>
+</head>
+<body>
+    <div class="main">
+        <div class="img"></div>
+        <h1>Muhammad Naufal Hasyim</h1>
+        <div class="text">Full Stack Web Developer,<strong>Indonesia</strong></div>
+        <P>Halo,Nama saya Muhammad Naufal Hasyim saya lahir di jakarta,19 Juni 2002 saya tinggal di villa bintaro indah ditangerang selatan saya sedang menjalani pendidikan S1 dan hobby saya adalah bermain game.</P>
+
+
+
+    </div>
+</body>
+</html>
+
